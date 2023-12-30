cf51e65c9430838385f19008264a208d5072236a
asimtimsina <79533233+asimtimsina@users.noreply.github.com>
2023-12-30 14:09:26 -0600

    add all
    add all docker-jenkins-integration

---
+++ src/test/java/com/example/dockerjenkinsdocker/DockerJenkinsDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dockerjenkinsdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerJenkinsDockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
